bcf9e1f0a1f27122f7d178dd4aefb1d87c68befc
nguyencuong1002 <bestarioncls.13@gmail.com>
2024-03-20 22:35:09 +0700

    feat: add file user-login.py (#1)

---
+++ app/src/user-login.py
@@ -0,0 +1,11 @@
+# def login_by_email(email, password):
+
+def login_by_email(email, password):
+    # Step 1: Check if the email exists in the database
+    print("Checking if the email exists in the database")
+    pass
+
+    # Step 2: Check if the password is correct
+    print("Checking if the password is correct")
+    pass
+
